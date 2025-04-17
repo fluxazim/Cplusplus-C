f51be520abe551fecb5d212b916ce4acfeedad73
Mohammed Azim <mohammedazim25@outlook.com>
2025-04-17 17:41:36 -0400

    created Person class first class example

---
+++ Live-Lec-1_04_17_2025/classes_1.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+class Person {
+
+private : 
+    string name;
+    int age;
+    bool gender; // 1 -> male  0 -> female
+public :
+
+//setters 
+void setName(const string& newName){ name  = newName;}
+void setAge(const int& nA){age = nA;}
+void setGender(const bool& nG){gender = nG;}
+
+//getters
+string getName(){ return name;}
+int getAge(){return age;}
+bool getGender(){return gender;}
+
+//constructors
+//parametrized constructors
+Person(const string& nname,int nage,bool  ngender)
+{
+    name = nname;
+    age = nage;
+    gender = ngender;
+}
+//default constructor
+Person()
+{
+    name = "Mohammed Azim";
+    age = 35;
+    gender = true;
+}
+//copy constructor
+Person(Person& P)
+{
+    name = P.name;
+    age = P.age;
+    gender = P.gender;
+}
+//Destructor
+virtual ~Person();
+
+};
+
+int main()
+{
+return 0;
+}
